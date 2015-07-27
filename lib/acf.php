b8f10557f093598041041b9b850d709de313e3bb
Alex <surge@aegirs-iMac.local>
2015-07-27 10:49:23 +1000

    Changes to ACF

--- lib/acf.php
+++ lib/acf.php
@@ -27,7 +27,7 @@ function my_acf_settings_dir( $dir ) {
     
 }
 // 3. Hide ACF field group menu item
-add_filter('acf/settings/show_admin', '__return_false');
+// add_filter('acf/settings/show_admin', '__return_false');
 
 
 // 4. Include ACF
--- templates/part-bugherd.php
+++ templates/part-bugherd.php
@@ -1,6 +1,1 @@
-<script type='text/javascript'>
-(function (d, t) {
-  var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];   bh.type = 'text/javascript';
-  bh.src = '//www.bugherd.com/sidebarv2.js?apikey=xky2desw3kuxszl4akbxbg';   s.parentNode.insertBefore(bh, s);
-  })(document, 'script');
-</script>
+<?php the_field('bugherd_code',get_id_from_slug('design-review')); ?>
